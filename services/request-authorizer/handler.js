4b1170984a58d631bd3cdc148fefccc7d2ea37d1
Ian King <ian.antking@gmail.com>
2021-09-21 09:22:25 +0100

    lamda authenticator returns IAm policy

--- services/request-authorizer/handler.js
+++ services/request-authorizer/handler.js
@@ -2,7 +2,17 @@ exports.run = async (event) => {
   const { PASSWORD } = process.env;
   const { authorization } = event.headers;
 
-  const response = { isAuthorized: PASSWORD === authorization };
-
-  return response;
+  return {
+    principalId: 'user',
+    policyDocument: {
+      Version: '2012-10-17',
+      Statement: [
+        {
+          Action: 'execute-api:Invoke',
+          Effect: PASSWORD === authorization ? 'Allow' : 'Deny',
+          Resource: event.routeArn,
+        },
+      ],
+    },
+  };
 };
