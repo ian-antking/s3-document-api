ce4c460b2db0f14942d0a8850d93e19897539c0c
Ian King <ian.antking@gmail.com>
2021-09-24 13:08:30 +0100

    documents must have a b64 encoded contant field

--- services/document-writer/src/schema/create-document-request.js
+++ services/document-writer/src/schema/create-document-request.js
@@ -1,8 +1,8 @@
 const Joi = require('joi');
 
 module.exports = Joi.object({
-  name: Joi.string()
-    .required(),
-  document: Joi.any()
-    .required(),
+  name: Joi.string().required(),
+  document: Joi.object({
+    content: Joi.string().base64().required(),
+  }),
 });
